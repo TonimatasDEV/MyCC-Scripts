0dd95f24683f5bfb688ef418347cf19b62de6bf3
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-08-05 22:20:43 +0200

    chore: Improve farmRecolector startup

--- farmRecolector.lua
+++ farmRecolector.lua
@@ -1,72 +1,80 @@
-if turtle.getFuelLevel() < 1000 then
-    if turtle.suckDown() then
-        turtle.refuel()
+while true do
+    if not redstone.getInput("left") then
+        sleep(0.5)
+        goto continue
     end
 
-    turtle.turnLeft()
-    turtle.drop()
-    turtle.turnRight()
-end
+    if turtle.getFuelLevel() < 1000 then
+        if turtle.suckDown() then
+            turtle.refuel()
+        end
 
-turtle.up()
-turtle.forward()
+        turtle.turnLeft()
+        turtle.drop()
+        turtle.turnRight()
+    end
 
-function recolect()
-    local success, data = turtle.inspectDown()
+    turtle.up()
+    turtle.forward()
 
-    if success then
-        if data.name == "minecraft:potatoes" then
-            local age = tonumber(data.state.age)
-            if age == 7 then
-                turtle.digDown()
-                turtle.placeDown()
+    function recolect()
+        local success, data = turtle.inspectDown()
+
+        if success then
+            if data.name == "minecraft:potatoes" then
+                local age = tonumber(data.state.age)
+                if age == 7 then
+                    turtle.digDown()
+                    turtle.placeDown()
+                end
             end
         end
     end
-end
 
-local forward = true
+    local forward = true
 
-for i = 1, 9 do
-    for i = 1, 8, 1 do
+    for i = 1, 9 do
+        for i = 1, 8, 1 do
+            recolect()
+            turtle.forward()
+        end
+
         recolect()
+
+        if forward then
+            turtle.turnLeft()
+            turtle.forward()
+            turtle.turnLeft()
+            forward = false
+        else
+            turtle.turnRight()
+            turtle.forward()
+            turtle.turnRight()
+            forward = true
+        end
+    end
+
+    for i = 1, 9 do
         turtle.forward()
     end
 
-    recolect()
+    turtle.turnLeft()
 
-    if forward then
-        turtle.turnLeft()
+
+    for i = 1, 9 do
         turtle.forward()
-        turtle.turnLeft()
-        forward = false
-    else
-        turtle.turnRight()
-        turtle.forward()
-        turtle.turnRight()
-        forward = true
     end
+
+    turtle.turnLeft()
+    turtle.down()
+
+    turtle.turnLeft()
+    for i = 1, 16 do
+        turtle.select(i)
+        turtle.drop()
+    end
+
+    turtle.select(1)
+    turtle.turnRight()
+    ::continue::
 end
-
-for i = 1, 9 do
-    turtle.forward()
-end
-
-turtle.turnLeft()
-
-
-for i = 1, 9 do
-    turtle.forward()
-end
-
-turtle.turnLeft()
-turtle.down()
-
-turtle.turnLeft()
-for i = 1, 16 do
-    turtle.select(i)
-    turtle.drop()
-end
-
-turtle.select(1)
-turtle.turnRight()
